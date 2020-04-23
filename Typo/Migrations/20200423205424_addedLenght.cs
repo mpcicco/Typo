dd82bf76be4e4017dd59c1f9ad9f2ebdbc802d9a
Enrico <Enrico@MacBook-Pro-di-Enrico.local>
2020-04-23 23:53:30 +0200

    Added word records

---
+++ Typo/Migrations/20200423205424_addedLenght.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Typo.Migrations
+{
+    public partial class addedLenght : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NLetters",
+                table: "Words",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "NLetters",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "NLetters",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "NLetters",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "NLetters",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Words",
+                keyColumn: "Id",
+                keyValue: 5L,
+                column: "NLetters",
+                value: 10);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NLetters",
+                table: "Words");
+        }
+    }
+}
